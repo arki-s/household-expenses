f66e59c5de442c0d9ca181a3f6454348ae9b4cb9
exec zsh <wind10cloud@gmail.com>
2023-07-27 13:21:16 +0900

    controller and route

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,2 +1,44 @@
 class BudgetsController < ApplicationController
+  def index
+    @user = current_user
+    @budgets = current_user.budgets
+  end
+
+  def new
+    @budget = Budget.new
+  end
+
+  def create
+    @budget = Budget.new
+    @user = current_user
+    @budget.user = @user
+    if @budget.save
+      redirect_to budgets_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def budget_params
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "budgets#index"
+  resources :budgets, only: [:show, :new, :create] do
+    resources :records, only: [:new, :create, :edit, :update]
+  end
+  resources :budgets, only: [:index, :edit, :update, :destroy]
+  resources :records, only: [:index, :destroy]
+
 end
